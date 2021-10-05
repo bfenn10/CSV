dd4a7000c61a483ab3010cf2ebe007afde9f38bd
Blake Fenn <blake_fenn1@baylor.edu>
2021-10-05 12:32:59 -0500

    sitka5 added

---
+++ sitka5.py
@@ -0,0 +1,49 @@
+import matplotlib.pyplot as plt
+import csv
+from datetime import datetime
+
+open_file = open("death_valley_2018_simple.csv", "r")
+
+csv_file = csv.reader(open_file, delimiter=',')
+
+header_row = next(csv_file)
+
+print(type(header_row))
+
+for index, column_header in enumerate(header_row):
+    print(index, column_header)
+
+# testing to convert date from string
+mydate = datetime.strptime('2018-07-01', '%Y-%m-%d')
+
+highs = []
+dates = []
+lows = []
+
+for row in csv_file:
+    try:
+        the_date = datetime.strptime(row[2], '%Y-%m-%d')
+        high = int(row[4])
+        low = int(row[5])
+    except ValueError:
+        print(f'Missing data for {the_date}')
+    else:
+        highs.append(int(row[4]))
+        lows.append(int(row[5]))
+        dates.append(the_date)
+
+print(highs)
+print(dates)
+
+
+plt.subplot(2, 1, 1)
+plt.plot(dates, highs, c='red')
+plt.title("Highs")
+
+plt.subplot(2, 1, 2)
+plt.plot(dates, lows, c='blue')
+plt.title('Lows')
+
+plt.subtitle("Highs and Lows of Sitka, Alaska")
+
+plt.show()
